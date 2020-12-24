abf7cf1412542ee7e1dee9d64349ba2bba0f944d
Taylor Thornton <thornrae@gmail.com>
2020-12-23 18:59:34 -0800

    upload 2 for deployment

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
 'use strict';
 
+//Step 1: bring in dependencies
+  //this allows us to read the .env file
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+//Step 2: set up your application
+const app = express();
+  // create PORT local request PORT 3000 need to create a port your server is going to listen on
+const PORT = process.env.PORT;
+app.use(cors());
+
+//Step 3: Routes
+app.get('/', (request, response) =>{
+  response.send('hello world');
+});
+
+//catch all route to display 404 error message/DNE
+app.use('*', (request, response)=>{
+  response.send('404 This page does not exist');
+});
+
+// Start our server
+app.listen(PORT, () => {
+  console.log(`Now listening on port, ${PORT}`);
+});
+
+
+
 
 
